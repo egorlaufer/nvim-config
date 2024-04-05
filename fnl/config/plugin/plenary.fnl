c5d272866c49dcd31daa5ea0e2a5975a16c05db1
Egor Laufer <laufer@hey.com>
2024-04-05 08:16:25 +0400

    lazy-specs: move plenary spec to module

---
+++ fnl/config/plugin/plenary.fnl
@@ -0,0 +1,2 @@
+{1 :nvim-lua/plenary.nvim :lazy false}
+
---
+++ lua/config/plugin/plenary.lua
@@ -0,0 +1,2 @@
+-- [nfnl] Compiled from fnl/config/plugin/plenary.fnl by https://github.com/Olical/nfnl, do not edit.
+return {"nvim-lua/plenary.nvim", lazy = false}
