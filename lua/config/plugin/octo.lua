3200151be2ba3a142eb150bb2487674cca1e2ecf
Egor Laufer <laufer@hey.com>
2024-04-05 07:33:37 +0400

    lazy-specs: move octo spec to module

--- lua/config/plugin/octo.lua
+++ lua/config/plugin/octo.lua
@@ -1,4 +1,5 @@
 -- [nfnl] Compiled from fnl/config/plugin/octo.fnl by https://github.com/Olical/nfnl, do not edit.
+local mod = ...
 local function config(plugin, opts)
   local ok_3f, octo = nil, nil
   local function _1_()
@@ -11,4 +12,28 @@ local function config(plugin, opts)
     return nil
   end
 end
-return {config = config}
+local function _3_(plugin_12_auto, opts_13_auto)
+  local start_14_auto = vim.loop.hrtime()
+  local fidget_3f_15_auto, fidget_16_auto = nil, nil
+  local function _4_()
+    return require("fidget")
+  end
+  fidget_3f_15_auto, fidget_16_auto = pcall(_4_)
+  local ok_3f_17_auto, res_18_auto = pcall(config, plugin_12_auto, opts_13_auto)
+  if ok_3f_17_auto then
+    if fidget_3f_15_auto then
+      local ms_19_auto = ((vim.loop.hrtime() - start_14_auto) / 1000000)
+      fidget_16_auto.notify((mod .. " in " .. ms_19_auto .. "ms"))
+    else
+    end
+    return true
+  else
+    if fidget_3f_15_auto then
+      fidget_16_auto.notify(("Failed configuring: " .. mod .. res_18_auto))
+    else
+      print(("Failed configuring: " .. mod .. res_18_auto))
+    end
+    return false
+  end
+end
+return {"pwntester/octo.nvim", config = _3_, dependencies = {{"nvim-tree/nvim-web-devicons"}, {"nvim-lua/plenary.nvim"}, {"nvim-telescope/telescope.nvim"}}}
