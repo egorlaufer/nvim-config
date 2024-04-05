4d4c232fbd0d9367869535e41dbf2c1cebcaa41b
Egor Laufer <laufer@hey.com>
2024-04-05 08:13:27 +0400

    lazy-specs: move vim-characterize spec to module

---
+++ fnl/config/plugin/vim-characterize.fnl
@@ -0,0 +1,2 @@
+{1 :tpope/vim-characterize :lazy false}
+
---
+++ lua/config/plugin/vim-characterize.lua
@@ -0,0 +1,2 @@
+-- [nfnl] Compiled from fnl/config/plugin/vim-characterize.fnl by https://github.com/Olical/nfnl, do not edit.
+return {"tpope/vim-characterize", lazy = false}
